f48886ba9986a6492b9845cec7aea393c786ef65
Ralpmike <ralphmike996@gmail.com>
2025-05-03 13:28:47 +0100

    let,const and var

---
+++ 01-Fundamentals-Part-1/starter/index.js
@@ -0,0 +1,12 @@
+const js = "amazing";
+// if (js === "amazing") alert("JavaScript is FUN!");
+
+40 + 8 - 23 - 10;
+
+let country, continent, population;
+
+country = "Nigeria";
+continent = "Africa";
+population = 200_000_000;
+
+console.log(country, continent, population);
