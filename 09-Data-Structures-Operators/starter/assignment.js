3f3baa97336d5a8360ccb448dc1fbba7a177204d
Ralpmike <ralphmike996@gmail.com>
2025-05-26 14:06:25 +0100

    assignment two completion

--- 09-Data-Structures-Operators/starter/assignment.js
+++ 09-Data-Structures-Operators/starter/assignment.js
@@ -261,6 +261,27 @@ TEST DATA FOR 6: Use players 'Davies', 'Muller', 'Lewandowski' and 'Kimmich'. Th
 GOOD LUCK 😀
 */
 
+//?Assignment #2
+/*Let's continue with our football betting app!
+
+1. Loop over the game.scored array and print each player name to the console, along with the goal number (Example: "Goal 1: Lewandowski")
+2. Use a loop to calculate the average odd and log it to the console (We already studied how to calculate averages, you can go check if you don't remember)
+3. Print the 3 odds to the console, but in a nice formatted way, exaclty like this:
+      Odd of victory Bayern Munich: 1.33
+      Odd of draw: 3.25
+      Odd of victory Borrussia Dortmund: 6.5
+Get the team names directly from the game object, don't hardcode them (except for "draw"). HINT: Note how the odds and the game objects have the same property names 😉
+
+BONUS: Create an object called 'scorers' which contains the names of the players who scored as properties, and the number of goals as the value. In this game, it will look like this:
+      {
+        Gnarby: 1,
+        Hummels: 1,
+        Lewandowski: 2
+      }
+
+GOOD LUCK 😀
+*/
+
 const game = {
   team1: 'Bayern Munich',
   team2: 'Borrussia Dortmund',
@@ -300,8 +321,55 @@ const game = {
     x: 3.25,
     team2: 6.5,
   },
+  averageOdd() {
+    let average = 0;
+    let length = Object.entries(this.odds).length;
+    for (const odd of Object.values(this.odds)) {
+      average += odd;
+    }
+
+    return average / length;
+  },
 };
 
+for (const [team, odd] of Object.entries(game.odds)) {
+  if (team === 'x') {
+    console.log(`Odd of draw: ${odd}`);
+  } else {
+    console.log(`Odd of victory ${game[team]}: ${odd}`);
+  }
+}
+
+const players = Object.entries(game.scored);
+
+console.log(players);
+
+for (const [i, player] of game.scored.entries()) {
+  console.log();
+  console.log(`Goal ${i + 1}: ${player}`);
+}
+
+const averageOdds = game.averageOdd?.();
+
+console.log(averageOdds);
+// {
+//   Gnarby: 1,
+//   Hummels: 1,
+//   Lewandowski: 2
+// }
+
+const scorers = {};
+
+for (const scorer of game.scored) {
+  scorers[scorer] = (scorers[scorer] || 0) + 1;
+  // if (scorers[scorer]) {
+  //   scorers[scorer] += 1;
+  // } else {
+  //   scorers[scorer] = 1;
+  // }
+}
+
+console.log(scorers);
 //* 1. Create one player array for each team
 
 const [player1, player2] = game.players;
--- 09-Data-Structures-Operators/starter/index.js
+++ 09-Data-Structures-Operators/starter/index.js
@@ -1,16 +1,16 @@
 'use strict';
 const weekDays = ['mon', 'tue', 'wed', 'thu', 'fri'];
 
-const Hours = {
+const openingHours = {
   [weekDays[3]]: {
     open: 12,
     close: 22,
   },
-  [weekDays[weekDays.length - 1]]: {
+  [weekDays[2]]: {
     open: 11,
     close: 23,
   },
-  [`Day-${1 + 5}`]: {
+  [weekDays[4]]: {
     open: 0, // Open 24 hours
     close: 24,
   },
@@ -23,8 +23,8 @@ const restaurant = {
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
   //? ES6 enhanced object literals
-  // openingHours: operingHours,
-  Hours,
+  openingHours,
+  // Hours,
 
   order(starterIndex, mainIndex) {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
@@ -55,207 +55,229 @@ restaurant.orderDelivery({
   starterIndex: 2,
 });
 
-let [main, , secondary] = restaurant.categories;
+//? Object keys: Property names
+const properties = Object.keys(openingHours);
+console.log(properties);
 
-console.log(main, secondary);
-
-//* Swapping variables in JavaScript can be done using destructuring assignment.
-// const temp = main;
-// main = secondary;
-// secondary = temp;
-
-// console.log(main, secondary, temp);
-
-[main, secondary] = [secondary, main];
-console.log(main, secondary);
-
-const [starter, mainCourse] = restaurant.order(2, 0); // ['Garlic Bread', 'Pizza']
-
-//? returns an array with the first element being the starter and the second being the main course
-console.log(starter, mainCourse);
-
-//? Nested destructuring
-
-const nested = [2, 4, [5, 6]];
-// const [i, , j] = nested; // i = 2, j = [5, 6]
-// console.log(i, j); // 2, [5, 6]
-
-const [i, , [j, k]] = nested; // i = 2, j = 5, k = 6
-console.log(i, j, k); // 2, 5, 6
-
-//? Default values in destructuring
-const [p = 1, q = 1, r = 1] = [8, 9];
-console.log(p, q, r); // 8, 9, 1
-
-//! Object destructuring example
-
-//? the order of the properties in the object does not matter
-
-const { name, openingHours, categories } = restaurant;
-
-//? to rename variables during destructuring, you can use the following syntax
-
-const {
-  name: restaurantName,
-  openingHours: hours,
-  categories: tags,
-} = restaurant;
-
-//? setting default values during destructuring
-
-const { menu = [], starterMenu: starters = [] } = restaurant;
-
-//? Mutating variables during destructuring
-let a = 111;
-let b = 999;
-
-const obj = { a: 23, b: 7, c: 14 };
-({ a, b } = obj); // destructuring assignment with mutation
-
-console.log(a, b);
-
-//? Nested object destructuring
-
-const {
-  fri: { open: o, close: c },
-} = openingHours; // destructuring the 'fri' property from 'openingHours'
-console.log(o, c); // { open: 11, close: 23 }
-
-//* Spread operator example
-//? The spread operator is used to unpack elements from an array or properties from an object.
-//*Spread operator is used to merge arrays or objects
-const arr = [7, 8, 9];
-const badNewArr = [1, 2, arr[0], arr[1], arr[2]]; // This creates a nested array
-const newArr = [1, 2, ...arr];
-
-console.log(badNewArr);
-console.log(newArr); // [1, 2, 7, 8, 9]
-
-const newMenu = [...restaurant.mainMenu, 'Gnocchi'];
-
-//? Copying arrays
-const mainMenuCopy = [...restaurant.mainMenu];
-console.log(mainMenuCopy); // ['Pizza', 'Pasta', 'Risotto']
-const menuCombined = [...restaurant.starterMenu, ...restaurant.mainMenu];
-console.log(menuCombined); // ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad', 'Pizza', 'Pasta', 'Risotto', 'Gnocchi']
-
-//?iterables: arrays, strings, maps, sets
-const str = 'Raphael';
-const letters = [...str, ' ', 'S.'];
-console.log(letters); // ['R', 'a', 'p', 'h', 'a', 'e', 'l', ' ', 'S.']
-
-console.log(...str);
-
-const ingredients = [
-  // prompt("Let's make pasta! Ingredient 1?"),
-  // prompt('Ingredient 2?'),
-  // prompt('Ingredient 3?'),
-];
-
-console.log(ingredients);
-
-restaurant.orderPasta(...ingredients); // Here we spread the ingredients array into the function call
-
-//? Objects
-const newRestaurant = { foundedIn: 1998, ...restaurant, founder: 'Giuseppe' };
-console.log(newRestaurant);
-
-//?copying objects using spread operator
-
-const restaurantCopy = { ...restaurant };
-console.log(restaurantCopy);
-restaurantCopy.name = 'Ristorante Roma';
-console.log(restaurantCopy.name); // Ristorante Roma
-console.log(restaurant.name); // Classico Italiano
-
-//? Rest operator
-
-//? The rest operator is used to collect multiple elements into an array.
-const arr2 = [1, 2, ...[3, 4]]; // Spread operator
-const [x, y, ...others] = [1, 2, 3, 4, 5]; // Rest operator
-console.log(x, y, others); // 1, 2, [3, 4, 5]
-
-//? Destructuring with rest operator
-const [pizza, , risotto, ...otherFood] = [
-  ...restaurant.mainMenu,
-  ...restaurant.starterMenu,
-];
-console.log(pizza, risotto, otherFood);
-
-//? Rest operator in object destructuring
-const { sat, ...weekdays } = restaurant.openingHours;
-console.log(sat, weekdays);
-
-//? Rest operator in function parameters
-const add = function (...numbers) {
-  let sum = 0;
-  for (let i = 0; i < numbers.length; i++) sum += numbers[i];
-  console.log(sum);
-};
-add(2, 3);
-add(5, 3, 7, 2);
-add(8, 2, 5, 3, 2, 1, 4);
-
-//? short circuiting with logical operators
-console.log('--- OR ---');
-//*OR(||) operator looks for the first truthy value
-console.log(3 || 'Raphael'); // 3
-
-//?falsy values: 0, '', undefined, null, NaN
-
-console.log('--- AND ---');
-
-//*AND(&&) operator looks for the first falsy value
-console.log('' && 'Raphael'); // ''
-
-if (restaurant.orderPizza) {
-  restaurant.orderPizza('mushrooms', 'spinach');
+for (const day of properties) {
+  console.log(day);
 }
 
-restaurant.orderPizza && restaurant.orderPizza('mushrooms', 'spinach');
-console.log('--- Nullish Coalescing Operator ---');
-//? The nullish coalescing operator (??) is used to provide a default value when the left-hand side is null or undefined, unlike the OR operator which considers all falsy values.
+//? Object values: Property values
+const values = Object.values(openingHours);
+console.log(values);
 
-restaurant.numGuests = 0; // simulating a case where numGuests is 0
-const guests1 = restaurant.numGuests || 10; // 10 because 0 is falsy
-console.log(guests1); // 10
-const guests2 = restaurant.numGuests ?? 10; // 10 because 0 is nullish
-console.log(guests2);
-//*Nullish: null and undefined (Not 0 or '')
+const entries = Object.entries(openingHours);
+console.log(entries);
 
-//? Logical assignment operators
+for (const [day, { open, close }] of entries) {
+  console.log(`On ${day} we open at ${open} and close at ${close}`);
+}
 
-const rest1 = {
-  name: 'Capri',
-  // numGuests: 20,
-  numGuests: 0, // simulating a case where numGuests is 0
-};
+// for (const [open, close] of Object.entries(values)) {
+//   console.log(open, close);
+// }
+// // let [main, , secondary] = restaurant.categories;
 
-const rest2 = {
-  name: 'La Piazza',
-  owner: 'Giovanni Rossi',
-};
+// console.log(main, secondary);
 
-//* Logical assignment operators combine logical operators with assignment.
-//? The logical OR assignment operator (||=) assigns a value only if the left-hand side is falsy.
+// //* Swapping variables in JavaScript can be done using destructuring assignment.
+// // const temp = main;
+// // main = secondary;
+// // secondary = temp;
 
-// rest2.numGuests = rest2.numGuests || 10; // 10 because rest2.numGuests is undefined
-// rest1.numGuests = rest1.numGuests || 10;
+// // console.log(main, secondary, temp);
 
-// rest2.numGuests ||= 10;
-// rest1.numGuests ||= 10;
+// [main, secondary] = [secondary, main];
+// console.log(main, secondary);
 
-//* The logical nullish assignment operator (??=) assigns a value only if the left-hand side is null or undefined.
-rest2.numGuests ??= 10; // rest2.numGuests is undefined, so it gets assigned 10
-rest1.numGuests ??= 10; // rest1.numGuests is 0, so it remains 0
+// const [starter, mainCourse] = restaurant.order(2, 0); // ['Garlic Bread', 'Pizza']
 
-// console.log(rest1);
-// console.log(rest2);
+// //? returns an array with the first element being the starter and the second being the main course
+// console.log(starter, mainCourse);
 
-//* The logical AND assignment operator (&&=) assigns a value only if the left-hand side is truthy.
-// rest1.owner = rest1.owner && 'Anonymous'; // rest1.owner is undefined, so it remains undefined
-// rest2.owner = rest2.owner && 'Anonymous'; // rest2.owner is 'Giovanni Rossi', so it gets assigned 'Anonymous'
-// console.log(rest1, rest2);
-rest1.owner &&= 'Anonymous'; // rest1.owner is undefined, so it remains undefined
-rest2.owner &&= 'Anonymous'; // rest2.owner is 'Giovanni Rossi', so it gets assigned 'Anonymous'
-console.log(rest1, rest2); // rest1.owner remains undefined, rest2.owner becomes 'Anonymous'
+// //? Nested destructuring
+
+// const nested = [2, 4, [5, 6]];
+// // const [i, , j] = nested; // i = 2, j = [5, 6]
+// // console.log(i, j); // 2, [5, 6]
+
+// const [i, , [j, k]] = nested; // i = 2, j = 5, k = 6
+// console.log(i, j, k); // 2, 5, 6
+
+// //? Default values in destructuring
+// const [p = 1, q = 1, r = 1] = [8, 9];
+// console.log(p, q, r); // 8, 9, 1
+
+// //! Object destructuring example
+
+// //? the order of the properties in the object does not matter
+
+// const { name, openingHours, categories } = restaurant;
+
+// //? to rename variables during destructuring, you can use the following syntax
+
+// const {
+//   name: restaurantName,
+//   openingHours: hours,
+//   categories: tags,
+// } = restaurant;
+
+// //? setting default values during destructuring
+
+// const { menu = [], starterMenu: starters = [] } = restaurant;
+
+// //? Mutating variables during destructuring
+// let a = 111;
+// let b = 999;
+
+// const obj = { a: 23, b: 7, c: 14 };
+// ({ a, b } = obj); // destructuring assignment with mutation
+
+// console.log(a, b);
+
+// //? Nested object destructuring
+
+// const {
+//   fri: { open: o, close: c },
+// } = openingHours; // destructuring the 'fri' property from 'openingHours'
+// console.log(o, c); // { open: 11, close: 23 }
+
+// //* Spread operator example
+// //? The spread operator is used to unpack elements from an array or properties from an object.
+// //*Spread operator is used to merge arrays or objects
+// const arr = [7, 8, 9];
+// const badNewArr = [1, 2, arr[0], arr[1], arr[2]]; // This creates a nested array
+// const newArr = [1, 2, ...arr];
+
+// console.log(badNewArr);
+// console.log(newArr); // [1, 2, 7, 8, 9]
+
+// const newMenu = [...restaurant.mainMenu, 'Gnocchi'];
+
+// //? Copying arrays
+// const mainMenuCopy = [...restaurant.mainMenu];
+// console.log(mainMenuCopy); // ['Pizza', 'Pasta', 'Risotto']
+// const menuCombined = [...restaurant.starterMenu, ...restaurant.mainMenu];
+// console.log(menuCombined); // ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad', 'Pizza', 'Pasta', 'Risotto', 'Gnocchi']
+
+// //?iterables: arrays, strings, maps, sets
+// const str = 'Raphael';
+// const letters = [...str, ' ', 'S.'];
+// console.log(letters); // ['R', 'a', 'p', 'h', 'a', 'e', 'l', ' ', 'S.']
+
+// console.log(...str);
+
+// const ingredients = [
+//   // prompt("Let's make pasta! Ingredient 1?"),
+//   // prompt('Ingredient 2?'),
+//   // prompt('Ingredient 3?'),
+// ];
+
+// console.log(ingredients);
+
+// restaurant.orderPasta(...ingredients); // Here we spread the ingredients array into the function call
+
+// //? Objects
+// const newRestaurant = { foundedIn: 1998, ...restaurant, founder: 'Giuseppe' };
+// console.log(newRestaurant);
+
+// //?copying objects using spread operator
+
+// const restaurantCopy = { ...restaurant };
+// console.log(restaurantCopy);
+// restaurantCopy.name = 'Ristorante Roma';
+// console.log(restaurantCopy.name); // Ristorante Roma
+// console.log(restaurant.name); // Classico Italiano
+
+// //? Rest operator
+
+// //? The rest operator is used to collect multiple elements into an array.
+// const arr2 = [1, 2, ...[3, 4]]; // Spread operator
+// const [x, y, ...others] = [1, 2, 3, 4, 5]; // Rest operator
+// console.log(x, y, others); // 1, 2, [3, 4, 5]
+
+// //? Destructuring with rest operator
+// const [pizza, , risotto, ...otherFood] = [
+//   ...restaurant.mainMenu,
+//   ...restaurant.starterMenu,
+// ];
+// console.log(pizza, risotto, otherFood);
+
+// //? Rest operator in object destructuring
+// const { sat, ...weekdays } = restaurant.openingHours;
+// console.log(sat, weekdays);
+
+// //? Rest operator in function parameters
+// const add = function (...numbers) {
+//   let sum = 0;
+//   for (let i = 0; i < numbers.length; i++) sum += numbers[i];
+//   console.log(sum);
+// };
+// add(2, 3);
+// add(5, 3, 7, 2);
+// add(8, 2, 5, 3, 2, 1, 4);
+
+// //? short circuiting with logical operators
+// console.log('--- OR ---');
+// //*OR(||) operator looks for the first truthy value
+// console.log(3 || 'Raphael'); // 3
+
+// //?falsy values: 0, '', undefined, null, NaN
+
+// console.log('--- AND ---');
+
+// //*AND(&&) operator looks for the first falsy value
+// console.log('' && 'Raphael'); // ''
+
+// if (restaurant.orderPizza) {
+//   restaurant.orderPizza('mushrooms', 'spinach');
+// }
+
+// restaurant.orderPizza && restaurant.orderPizza('mushrooms', 'spinach');
+// console.log('--- Nullish Coalescing Operator ---');
+// //? The nullish coalescing operator (??) is used to provide a default value when the left-hand side is null or undefined, unlike the OR operator which considers all falsy values.
+
+// restaurant.numGuests = 0; // simulating a case where numGuests is 0
+// const guests1 = restaurant.numGuests || 10; // 10 because 0 is falsy
+// console.log(guests1); // 10
+// const guests2 = restaurant.numGuests ?? 10; // 10 because 0 is nullish
+// console.log(guests2);
+// //*Nullish: null and undefined (Not 0 or '')
+
+// //? Logical assignment operators
+
+// const rest1 = {
+//   name: 'Capri',
+//   // numGuests: 20,
+//   numGuests: 0, // simulating a case where numGuests is 0
+// };
+
+// const rest2 = {
+//   name: 'La Piazza',
+//   owner: 'Giovanni Rossi',
+// };
+
+// //* Logical assignment operators combine logical operators with assignment.
+// //? The logical OR assignment operator (||=) assigns a value only if the left-hand side is falsy.
+
+// // rest2.numGuests = rest2.numGuests || 10; // 10 because rest2.numGuests is undefined
+// // rest1.numGuests = rest1.numGuests || 10;
+
+// // rest2.numGuests ||= 10;
+// // rest1.numGuests ||= 10;
+
+// //* The logical nullish assignment operator (??=) assigns a value only if the left-hand side is null or undefined.
+// rest2.numGuests ??= 10; // rest2.numGuests is undefined, so it gets assigned 10
+// rest1.numGuests ??= 10; // rest1.numGuests is 0, so it remains 0
+
+// // console.log(rest1);
+// // console.log(rest2);
+
+// //* The logical AND assignment operator (&&=) assigns a value only if the left-hand side is truthy.
+// // rest1.owner = rest1.owner && 'Anonymous'; // rest1.owner is undefined, so it remains undefined
+// // rest2.owner = rest2.owner && 'Anonymous'; // rest2.owner is 'Giovanni Rossi', so it gets assigned 'Anonymous'
+// // console.log(rest1, rest2);
+// rest1.owner &&= 'Anonymous'; // rest1.owner is undefined, so it remains undefined
+// rest2.owner &&= 'Anonymous'; // rest2.owner is 'Giovanni Rossi', so it gets assigned 'Anonymous'
+// console.log(rest1, rest2); // rest1.owner remains undefined, rest2.owner becomes 'Anonymous'
