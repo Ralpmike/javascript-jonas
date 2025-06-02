81474c040cce9f83f58eee01a1f63256d7aaffbd
Ralpmike <ralphmike996@gmail.com>
2025-06-02 13:04:05 +0100

    value verses reference

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -29,3 +29,41 @@ createBooking('LH123', 2);
 createBooking('LH123', 5);
 
 // console.log(bookings);
+
+const junkFoods = ['🍈', '🍕', '🍅', '🍌', '🍇', '🍑'];
+
+console.log(junkFoods);
+
+console.log(junkFoods.at(-1));
+
+{
+  const flight = 'LH234';
+  const jonas = {
+    name: 'Jonas Schmedtmann',
+    passport: 24739479284,
+  };
+
+  const checkIn = function (flightNum, passenger) {
+    flightNum = 'LH999';
+    passenger.name = 'Mr. ' + passenger.name;
+
+    if (passenger.passport === 24739479284) {
+      alert('Checked in');
+    } else {
+      alert('Wrong passport!');
+    }
+  };
+
+  // checkIn(flight, jonas);
+  // console.log(flight);
+  // console.log(jonas);
+
+  const newPassport = function (person) {
+    person.passport = Math.trunc(Math.random() * 1000000000);
+  };
+
+  newPassport(jonas);
+  checkIn(flight, jonas);
+
+  //? passing by value and passing by reference
+}
