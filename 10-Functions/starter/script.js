fb9b04399ec2632fb0467ef175b9acd09ea28864
Ralpmike <ralphmike996@gmail.com>
2025-06-05 15:45:53 +0100

    javaScript closures with practical examples

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -343,3 +343,41 @@ booker();
 booker();
 
 console.dir(booker);
+//? Closure continuation
+
+let f;
+
+const g = function () {
+  const a = 23;
+  f = function () {
+    console.log(a * 2);
+  };
+};
+
+const h = function () {
+  const b = 777;
+  f = function () {
+    console.log(b * 2);
+  };
+};
+
+g();
+f();
+h();
+f();
+// console.dir(f);
+
+//Example 1
+const boardPassengers = function (n, wait) {
+  const perGroup = n / 3;
+
+  setTimeout(function () {
+    console.log(`We are now boarding all ${n} passengers`);
+    console.log(`There are 3 groups, each with ${perGroup} passengers`);
+  }, wait * 1000);
+
+  console.log(`Will start boarding in ${wait} seconds`);
+};
+
+const pergroup = 1000;
+boardPassengers(180, 3);
