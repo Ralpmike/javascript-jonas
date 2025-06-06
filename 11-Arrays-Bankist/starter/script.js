c766cae1a1499064385d2f055d969989ffd980b9
Ralpmike <ralphmike996@gmail.com>
2025-06-06 16:03:17 +0100

    array at() methode

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -105,3 +105,13 @@ console.log([...arr, ...arr2]);
 
 //? JOIN method: does not modify the original array
 console.log(letters.join(' - '));
+
+{
+  //? AT method: does not modify the original array
+  const arr = [23, 11, 64];
+  console.log(arr[0]);
+  console.log(arr.at(0));
+  console.log(arr[arr.length - 1]);
+  console.log(arr.slice(-1)[0]);
+  console.log(arr.at(-1));
+}
