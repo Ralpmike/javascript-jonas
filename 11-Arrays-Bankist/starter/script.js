c57ca7a8672820703bc08629537d92ca04d73e9c
Ralpmike <ralphmike996@gmail.com>
2025-06-09 16:58:24 +0100

    forEach on maps and sets

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -137,3 +137,24 @@ console.log(letters.join(' - '));
     }
   });
 }
+
+//? forEach with maps and sets
+
+{
+  //* Map
+  const currencies = new Map([
+    ['USD', 'United States dollar'],
+    ['EUR', 'Euro'],
+    ['GBP', 'Pound sterling'],
+  ]);
+
+  currencies.forEach(function (value, key, map) {
+    console.log(`${key}: ${value}`);
+  });
+
+  const currenciesUnique = new Set(['USD', 'GBP', 'USD', 'EUR', 'EUR']);
+  console.log(currenciesUnique);
+  currenciesUnique.forEach(function (value, _, set) {
+    console.log(`${value}: ${value}`);
+  });
+}
