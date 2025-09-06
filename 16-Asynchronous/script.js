9a99fc0221247a95c210b6048ded7f56bf279032
Ralpmike <ralphmike996@gmail.com>
2025-09-06 20:35:07 +0100

    CallbackHell

--- 16-Asynchronous/script.js
+++ 16-Asynchronous/script.js
@@ -21,45 +21,65 @@ const countriesContainer = document.querySelector('.countries');
 //     const data = await res.json();
 
 //     console.log(data.slice(0, 10));
-    
+
 //   } catch (error) {
 //     console.log('Error', error);
-    
+
 //   }
 // }
 
-
 // fetchCountries()
 
+// function getCountryData(country) {
 
+//   const request = new XMLHttpRequest();
 
-function getCountryData(country) {
-  
-  const request = new XMLHttpRequest();
-  
-  request.open('GET', `https://restcountries.com/v2/name/${country}`);
-  
-  request.setRequestHeader('Content-Type', 'application/json');
-  
-  request.send();
-  
-  request.addEventListener('load', function () {
-    const [data] = JSON.parse(this.responseText);
-    console.log(data);
+//   request.open('GET', `https://restcountries.com/v2/name/${country}`);
 
-    const {currencies} = data;
-    // console.log(currencies);
-  
-    const html = `
-      <article class="country">
+//   request.setRequestHeader('Content-Type', 'application/json');
+
+//   request.send();
+
+//   request.addEventListener('load', function () {
+//     const [data] = JSON.parse(this.responseText);
+//     console.log(data);
+
+//     const {currencies} = data;
+//     // console.log(currencies);
+
+//     const html = `
+//       <article class="country">
+//             <img class="country__img" src=${data.flag}
+//     }/>
+//             <div class="country__data">
+//               <h3 class="country__name">${data.name}</h3>
+//               <h4 class="country__region">${data.region}</h4>
+//               <p class="country__row"><span>👫</span>${(data.population / 1000000).toFixed(
+//                 2
+//               )}</p>
+//               <p class="country__row"><span>🗣️</span>${data.languages.eng}</p>
+//               <p class="country__row"><span>💰</span>(${
+//                 data.currencies[0].code
+//               }) ${data.currencies[0].name}</p>
+//             </div>
+//           </article>
+//     `;
+
+//     countriesContainer.insertAdjacentHTML('beforeend', html);
+//     countriesContainer.style.opacity = 1;
+//   });
+// }
+const renderCountry = function (data, className = '') {
+  const html = `
+      <article class="country ${className}">
             <img class="country__img" src=${data.flag}
     }/>
             <div class="country__data">
               <h3 class="country__name">${data.name}</h3>
               <h4 class="country__region">${data.region}</h4>
-              <p class="country__row"><span>👫</span>${(data.population / 1000000).toFixed(
-                2
-              )}</p>
+              <p class="country__row"><span>👫</span>${(
+                data.population / 1000000
+              ).toFixed(2)}</p>
               <p class="country__row"><span>🗣️</span>${data.languages.eng}</p>
               <p class="country__row"><span>💰</span>(${
                 data.currencies[0].code
@@ -67,13 +87,50 @@ function getCountryData(country) {
             </div>
           </article>
     `;
-  
-    countriesContainer.insertAdjacentHTML('beforeend', html);
-    countriesContainer.style.opacity = 1;
+
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+};
+
+function getCountryAndNeighbourData(country) {
+  //?AJAX call country 1
+  const request = new XMLHttpRequest();
+
+  request.open('GET', `https://restcountries.com/v2/name/${country}`);
+
+  request.setRequestHeader('Content-Type', 'application/json');
+
+  request.send();
+
+  request.addEventListener('load', function () {
+    const [data] = JSON.parse(this.responseText);
+    console.log(data);
+
+    //?Render country
+    renderCountry(data);
+
+    //?Get neighbour country
+    const neighbour = data.borders?.[0];
+
+    if(!neighbour) return;
+
+    //?AJAX call country 2
+    const request2 = new XMLHttpRequest();
+    request2.open('GET', `https://restcountries.com/v2/alpha/${neighbour}`);
+    request2.setRequestHeader('Content-Type', 'application/json');
+    request2.send();
+
+    request2.addEventListener('load', function(){
+      const data2 = JSON.parse(this.responseText);
+      console.log('neighbour', data2);
+      
+      renderCountry(data2, "neighbour");
+    })
+
+
   });
 }
 
-
-getCountryData('nigeria');
-getCountryData('usa');
-getCountryData('germany');
+// getCountryData('nigeria');
+// getCountryData('usa');
+getCountryAndNeighbourData('portugal');
