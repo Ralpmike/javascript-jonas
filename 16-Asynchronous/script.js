2d5b04bafec2c0b3e58315ebd223e9af9bb3a88c
Ralpmike <ralphmike996@gmail.com>
2025-09-07 18:15:53 +0100

    chaining promises

--- 16-Asynchronous/script.js
+++ 16-Asynchronous/script.js
@@ -143,7 +143,20 @@ const renderCountry = function (data, className = '') {
 const getCountryData = function(country){
   fetch(`https://restcountries.com/v2/name/${country}`)
   .then(response => response.json())
-  .then(data => renderCountry(data[0]));
+  .then(data => {
+    renderCountry(data[0]);
+    const neighbour = data[0].borders?.[0];
+
+    if(!neighbour) return
+
+//?country 2
+    return fetch(`https://restcountries.com/v2/alpha/${neighbour}`)
+  })
+  .then(response => response.json())
+  .then(data => {
+    renderCountry(data, 'neighbour')
+  })
+
 }
 
-getCountryData('portugal');
+getCountryData('nigeria');
