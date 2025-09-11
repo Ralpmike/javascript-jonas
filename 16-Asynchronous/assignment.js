7da837b199d5cf856337a2fe912172c16bc81fab
Ralpmike <ralphmike996@gmail.com>
2025-09-11 17:13:31 +0100

    asynchronous javascript

--- 16-Asynchronous/assignment.js
+++ 16-Asynchronous/assignment.js
@@ -16,8 +16,8 @@ Here are your tasks:
 
 PART 1
 1. Create a function 'whereAmI' which takes as inputs a latitude value (lat) and a longitude value (lng) (these are GPS coordinates, examples are below).
-2. Do 'reverse geocoding' of the provided coordinates. Reverse geocoding means to convert coordinates to a meaningful location, like a city and country name. Use this API to do reverse geocoding: https://geocode.xyz/api.
-The AJAX call will be done to a URL with this format: https://geocode.xyz/52.508,13.381?geoit=json. Use the fetch API and promises to get the data. Do NOT use the getJSON function we created, that is cheating 😉
+2. Do 'reverse geocoding' of the provided coordinates. Reverse geocoding means to convert coordinates to a meaningful location, like a city and country name. Use this API to do reverse geocoding: https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lng}.
+The AJAX call will be done to a URL with this format: https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=30&longitude. Use the fetch API and promises to get the data. Do NOT use the getJSON function we created, that is cheating 😉
 3. Once you have the data, take a look at it in the console to see all the attributes that you recieved about the provided location. Then, using this data, log a messsage like this to the console: 'You are in Berlin, Germany'
 4. Chain a .catch method to the end of the promise chain and log errors to the console
 5. This API allows you to make only 3 requests per second. If you reload fast, you will get this error with code 403. This is an error with the request. Remember, fetch() does NOT reject the promise in this case. So create an error to reject the promise yourself, with a meaningful error message.
@@ -98,7 +98,7 @@ const whereAmI = function (lat, lng) {
     console.log('data', data);
     renderCountry(data[0]);
 
-    const neighbour = data[0]?.borders[0];
+    const neighbour = data[0]?.borders?.[0] ?? data[1]?.borders?.[0];
     console.log('neighbour', neighbour);
     
 
@@ -121,3 +121,6 @@ const whereAmI = function (lat, lng) {
 
 
 whereAmI(40, -76);
+whereAmI(52.508, 13.381);
+whereAmI(19.037, 72.873);
+whereAmI(-33.933, 18.474);
